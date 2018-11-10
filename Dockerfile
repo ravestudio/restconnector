243a4e6a043939ae3541a2b3fce53dee42df0fc9
ravestudio <ravestudio@mail.ru>
2018-11-10 22:30:52 +0600

    ee

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM microsoft/dotnet:sdk
+WORKDIR /app
+
+# copy csproj and restore as distinct layers
+COPY *.csproj ./
+RUN dotnet restore
+
+# copy and build everything else
+COPY . ./
+RUN dotnet publish -c Release -o out
+ENTRYPOINT ["dotnet", "out/restconnector.dll"]
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace restconnector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    string url = "https://jsonplaceholder.typicode.com/posts/1";
+                    var response = httpClient.GetAsync(url).Result;
+                    string jsonResult = response.Content.ReadAsStringAsync().Result;
+
+                    Console.WriteLine(jsonResult);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
